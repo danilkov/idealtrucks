6bd1f5afd4bd3a0ef10ef983ed234db9f1d6f057
danilkov <bcd9D=ec$>
2015-04-28 09:16:05 -0400

    Stupid copy-paste error

--- src/Controller/API/AuthController.php
+++ src/Controller/API/AuthController.php
@@ -55,7 +55,7 @@ class AuthController extends APIController {
 //        }
     }
 
-     public function tokenRefresh() {
+     public function verifyEmail() {
         // TODO: Implement
      }
 }
