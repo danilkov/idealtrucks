9868781aad149c6758c47b8706ed28e5a48594ee
danilkov <bcd9D=ec$>
2015-05-12 08:23:19 -0400

    WIP

--- webroot/modules/directives/password-strength.js
+++ webroot/modules/directives/password-strength.js
@@ -11,64 +11,52 @@
             link: function (scope, element, attributes, control) {
 
             var checker = {
-                colors: ['#F00', '#F90', '#FF0', '#9F0', '#0F0'],
-                measureStrength: function (p) {
-                    var _force = 0;
+                measureStrength: function(password) {
+                    var strength = -1;
 
-                    var _lowerLetters = /[a-z]+/.test(p);
-                    var _upperLetters = /[A-Z]+/.test(p);
-                    var _numbers = /[0-9]+/.test(p);
-                    var _symbols = /[!-\/|:-@|\[-`|\{-~]/g.test(p);
-
-                    /*var _flags = [_lowerLetters, _upperLetters, _numbers, _symbols];
-                    var _passedMatches = $.grep(_flags, function (el) { return el === true; }).length;
-
-                    _force += 2 * p.length + ((p.length >= 10) ? 1 : 0);
-                    _force += _passedMatches * 10;
-
-                    // penality (short password)
-                    _force = (p.length <= 6) ? Math.min(_force, 10) : _force;
-
-                    // penality (poor variety of characters)
-                    _force = (_passedMatches == 1) ? Math.min(_force, 10) : _force;
-                    _force = (_passedMatches == 2) ? Math.min(_force, 20) : _force;
-                    _force = (_passedMatches == 3) ? Math.min(_force, 40) : _force;
-
-                    return _force;*/
-                    return 10;
-                },
-                getColor: function (s) { // TODO: optimize?
-                    var idx = 0;
-                    if (s <= 10) { idx = 0; }
-                    else if (s <= 20) { idx = 1; }
-                    else if (s <= 30) { idx = 2; }
-                    else if (s <= 40) { idx = 3; }
-                    else { idx = 4; }
-
-                    return { idx: idx + 1, col: this.colors[idx] };
-                }
-            };
-            scope.strengthStyle = function(level) {
-                return "strength_" + level; // FIXME: should be based on strength and level
-            };
-
-            scope.$watch(attributes.checkStrength, function () {
-                if (scope.pw !== "") {
-                    var strength = checker.measureStrength(scope.pw);
-                    var c = checker.getColor(strength);
-                    var ul = element.find('ul');
-                    ul.css({"display": "inline"});
-
-                    var lis = ul.find('li');
-                    lis.css({"background": "#DDD"});
-                    for(var i = 0; i < c.idx; i++) {
-                        angular.element(lis[i]).css({"background": c.col});
+                    if(password) {
+                        if(/[a-z]+/.test(password)) { // lower-case letters
+                            strength++;
+                        }
+                        if(/[A-Z]+/.test(password)) { // upper-case letters
+                            strength++;
+                        }
+                        if(/[0-9]+/.test(password)) { // numbers
+                            strength++;
+                        }
+                        if(/[^a-zA-Z\d]/.test(password)) { // non alpha-numeric
+                            strength++;
+                        }
+                        if(password.length > 5) {
+                            strength++;
+                            if(password.length > 9) {
+                                strength++;
+                            }
+                        }
                     }
-                    //lis.slice(0, c.idx);
-                    //    .css({"background": c.col});
-                } else {
-                    element.css({"display": "none"});
-                }
+
+                    return strength;
+                };
+
+                scope.strengthStyle = function(level) {
+                    if(level <= scope.passwordStrength) {
+                        return "strength_" + scope.passwordStrength;
+                    }
+                    else {
+                        return null; // TODO: Verify if null can be returned
+                    }
+                };
+
+                scope.$watch(attributes.checkStrength, function () {
+                    var password = scope.$eval(attributes.ngModel);
+                    if (password && password !== "") {
+                        scope.passwordStrength = checker.measureStrength(password);
+                        element.css({"display": "inline"});
+                    }
+                    else {
+                        scope.passwordStrength = -1;
+                        element.css({"display": "none"});
+                    }
                 });
             },
             template: '<ul id="strength"><li ng-class="strengthStyle(0)"></li><li ng-class="strengthStyle(1)"></li><li ng-class="strengthStyle(2)"></li><li ng-class="strengthStyle(3)"></li><li ng-class="strengthStyle(4)"></li></ul>'
