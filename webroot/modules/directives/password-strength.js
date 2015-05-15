becf306caec115f4f6f4b5894b0e599d69b017a8
Dmitry Danilkov <dmitry.danilkov@gmail.com>
2015-05-15 10:16:59 -0400

    WIP

--- webroot/modules/directives/password-strength.js
+++ webroot/modules/directives/password-strength.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
     'use strict';
 
     angular.module('directives.passwordStrength', []).
@@ -7,35 +7,38 @@
             restrict: 'E',
             scope: true,
             replace: true,
-            //require: 'ngModel',
-            link: function (scope, element, attributes, control) {
+            require: 'ngModel',
+            link: function(scope, element, attributes, control) {
 
-            var checker = {
-                measureStrength: function(password) {
-                    var strength = -1;
+                var checker = {
+                    measureStrength: function(password) {
+                        var strength = -1;
 
-                    if(password) {
-                        if(/[a-z]+/.test(password)) { // lower-case letters
-                            strength++;
-                        }
-                        if(/[A-Z]+/.test(password)) { // upper-case letters
-                            strength++;
-                        }
-                        if(/[0-9]+/.test(password)) { // numbers
-                            strength++;
-                        }
-                        if(/[^a-zA-Z\d]/.test(password)) { // non alpha-numeric
-                            strength++;
-                        }
-                        if(password.length > 5) {
-                            strength++;
-                            if(password.length > 9) {
+                        if(password) {
+                            if(/[a-z]+/.test(password)) { // lower-case letters
+                                strength++;
+                            }
+                            if(/[A-Z]+/.test(password)) { // upper-case letters
+                                strength++;
+                            }
+                            if(/[0-9]+/.test(password)) { // numbers
+                                strength++;
+                            }
+                            if(/[^a-zA-Z\d]/.test(password)) { // non alpha-numeric
+                                strength++;
+                            }
+                            if(password.length < 7) {
+                                if(strength > 0) {
+                                    strength--;
+                                }
+                            }
+                            else if(password.length > 9) {
                                 strength++;
                             }
                         }
-                    }
 
-                    return strength;
+                        return strength;
+                    }
                 };
 
                 scope.strengthStyle = function(level) {
@@ -43,19 +46,21 @@
                         return "strength_" + scope.passwordStrength;
                     }
                     else {
-                        return null; // TODO: Verify if null can be returned
+                        return null;
                     }
                 };
 
-                scope.$watch(attributes.checkStrength, function () {
+                scope.$watch(attributes.ngModel, function () {
                     var password = scope.$eval(attributes.ngModel);
-                    if (password && password !== "") {
+                    if(password && password !== "") {
                         scope.passwordStrength = checker.measureStrength(password);
                         element.css({"display": "inline"});
+                        control.$setValidity("passwordTooWeek", scope.passwordStrength >= 2);
                     }
                     else {
                         scope.passwordStrength = -1;
                         element.css({"display": "none"});
+                        control.$setValidity("passwordTooWeek", true);
                     }
                 });
             },
